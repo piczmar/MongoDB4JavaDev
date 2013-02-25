92bc613721004e36ee9974847f899177cd76269c
Marcin Piczkowski <piczmar@wp.pl>
2013-02-26 00:24:09 +0100

    added Spark framework

---
+++ src/main/java/com/tengen/HelloWorldSparkStyle.java
@@ -0,0 +1,21 @@
+package com.tengen;
+
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+/**
+ * Run this and go to URL: http://localhost:4567/ in your browser
+ */
+public class HelloWorldSparkStyle {
+    public static void main(String[] args) {
+        Spark.get(new Route("/") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello World from Spark!";
+            }
+        });
+    }
+}
