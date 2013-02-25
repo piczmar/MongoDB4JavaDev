a7bea208e5463dbfe8a6219ac2b19e9cb44f2bb5
Marcin Piczkowski <piczmar@wp.pl>
2013-02-26 00:10:46 +0100

    First Mongo search from java, before you run src/main/java/com/tengen/HelloWorldMongoDBStyle.java create new mongo db: >mongo
    use course
    
    db.hello.save({'name':'MongoDB'})

---
+++ src/main/java/com/tengen/HelloWorldMongoDBStyle.java
@@ -0,0 +1,23 @@
+package com.tengen;
+
+
+import com.mongodb.*;
+
+import java.net.UnknownHostException;
+
+public class HelloWorldMongoDBStyle {
+    public static void main(String[] args) throws UnknownHostException {
+        // create logical connection to Mongo cluster
+        MongoClient client = new MongoClient(new ServerAddress("localhost", 27017));
+
+        // create database
+        DB db = client.getDB("course");
+
+        // get db collection (like a table in RDBMS)
+        DBCollection collection = db.getCollection("hello");
+
+        DBObject document = collection.findOne();
+
+        System.out.println(document);
+    }
+}
